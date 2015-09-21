748b326fa20a0d66e443ac7adedbdb4db60ccae7
aajjww <1947764221@qq.com>
2015-09-21 09:31:56 +0800

    001

---
+++ Haircut/Haircut/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Haircut
+//
+//  Created by ajw on 15/9/21.
+//  Copyright (c) 2015年 ajw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
